9c02c289ce137a717ecd287395b63c5f6bfb1662
Dejan Vjestica <dvjestica@gmail.com>
2026-03-05 15:14:03 +0100

    Allow images from strapi

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  staticPageGenerationTimeout: 500,
+  output: 'standalone',
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '1337',
+        pathname: '/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tranquil-boot-edf3c61b5c.strapiapp.com',
+        pathname: '/uploads/**',
+      },
+    ],
+  }
 };
 
 export default nextConfig;

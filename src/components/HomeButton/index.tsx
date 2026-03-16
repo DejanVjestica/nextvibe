42b153f02971329a4e3769d984dd0e4008edbf02
Dejan Vjestica <dvjestica@gmail.com>
2026-03-16 16:28:08 +0100

    Locilize home button

--- src/components/HomeButton/index.tsx
+++ src/components/HomeButton/index.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { Locale } from "@/i18n-config";
+import { i18nConfig, Locale } from "@/i18n-config";
 import { getLocalizedPath } from "@/lib/i18n-utils";
 import { useParams } from "next/navigation";
 import { NavLink } from "../NavLink";
 
 export const HomeButton = () => {
   const params = useParams();
+  const { defaultLocale } = i18nConfig;
 
   const currentLocale: Locale = params.locale as Locale;
+  const ariaLabel = currentLocale === defaultLocale ? "Zur Startseite" : "Go to home page";
 
-  return <NavLink href={getLocalizedPath("/", currentLocale)} label="Nextvibe" ariaLabel="Go to home page"></NavLink>;
+  return <NavLink href={getLocalizedPath("/", currentLocale)} label="Nextvibe" ariaLabel={ariaLabel}></NavLink>;
 };

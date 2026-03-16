d143c964187a56702394043e0bb00fcbf411569e
Dejan Vjestica <dvjestica@gmail.com>
2026-03-16 16:33:07 +0100

    Fix page styles

--- src/app/[locale]/[...slug]/page.tsx
+++ src/app/[locale]/[...slug]/page.tsx
@@ -1,13 +1,9 @@
 import { Locale } from "@/i18n-config";
 
-export default async function Home({
-  params,
-}: {
-  params: Promise<{ slug: Locale }>;
-}) {
+export default async function Home({ params }: { params: Promise<{ slug: Locale }> }) {
   const { slug } = await params;
   return (
-    <div className="max-w-container mx-auto ">
+    <div className="max-w-container mx-auto p-8">
       <h1>Locale main: {slug}</h1>
     </div>
   );

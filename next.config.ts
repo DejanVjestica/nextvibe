cb6f0cf658e96e115019c9f78d4802e512cce012
Dejan Vjestica <dvjestica@gmail.com>
2026-03-28 18:31:14 +0100

    Add custom image loader

--- next.config.ts
+++ next.config.ts
@@ -3,23 +3,25 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   staticPageGenerationTimeout: 500,
-  output: 'standalone',
+  output: "standalone",
 
   images: {
+    loader: "custom",
+    loaderFile: "./src/lib/strapi-loader.ts",
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'tranquil-boot-edf3c61b5c.strapiapp.com',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "**.strapiapp.com",
+        pathname: "/uploads/**",
       },
     ],
-  }
+  },
 };
 
 export default nextConfig;
---
+++ src/lib/strapi-loader.ts
@@ -0,0 +1,27 @@
+import { ImageLoader, ImageLoaderProps } from "next/image";
+
+const strapiLoader: ImageLoader = ({ src, width, quality }: ImageLoaderProps) => {
+  const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
+  if (!STRAPI_URL) throw new Error("NEXT_PUBLIC_STRAPI_URL is not defined");
+
+  const path = src.startsWith("http") ? new URL(src).pathname : src;
+
+  const prefix = imagePrefix(width);
+
+  const srcParts = path.split("/");
+  const filename = srcParts.pop();
+  const directory = srcParts.join("/");
+
+  const finalPath = prefix ? `${directory}/${prefix}_${filename}` : path;
+  return `${STRAPI_URL}${finalPath}?w=${width}&q=${quality || 75}`;
+};
+
+const imagePrefix = (width: number): string | null => {
+  if (width <= 245) return "thumbnail";
+  if (width <= 500) return "small";
+  if (width <= 750) return "medium";
+  if (width <= 1000) return "large";
+  return null;
+};
+
+export default strapiLoader;

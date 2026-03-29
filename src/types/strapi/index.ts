66464aded2e035d13494476e7cdadcc4e7979e92
Dejan Vjestica <dvjestica@gmail.com>
2026-03-29 17:52:26 +0200

    Extract block types from strapi

--- src/types/strapi/index.ts
+++ src/types/strapi/index.ts
@@ -1,5 +1,7 @@
-import { Schema } from "@strapi/types";
-import { ApiArticleArticle, ApiAuthorAuthor, ApiCategoryCategory, ApiAboutAbout } from "./generated/contentTypes";
+import { StrapiMedia } from "@/types/strapi/media";
+import { ApiArticleArticle, ApiAuthorAuthor, ApiCategoryCategory, ApiAboutAbout } from "@/types/strapi/generated/contentTypes";
+
+import { SharedMedia, SharedQuote, SharedRichText, SharedSlider } from "./generated/components";
 
 export type GetAttributes<T> = T extends { attributes: infer A } ? A : never;
 
@@ -7,6 +9,28 @@ export type StrapiItem<T> = {
   id: number;
   documentId: string;
 } & GetAttributes<T>;
+
+export type BlockItem<T, C extends string> = GetAttributes<T> & {
+  id: number;
+  __component: C;
+};
+
+export type Article = StrapiItem<ApiArticleArticle>;
+export type Author = StrapiItem<ApiAuthorAuthor>;
+export type Category = StrapiItem<ApiCategoryCategory>;
+export type About = Omit<StrapiItem<ApiAboutAbout>, "blocks"> & {
+  blocks: ContentBlock[];
+};
+
+export type ComponentMedia = Omit<BlockItem<SharedMedia, "shared.media">, "file"> & {
+  file: StrapiMedia;
+};
+
+export type ComponentQuote = BlockItem<SharedQuote, "shared.quote">;
+export type ComponentRichText = BlockItem<SharedRichText, "shared.rich-text">;
+export type ComponentSlider = BlockItem<SharedSlider, "shared.slider">;
+
+export type ContentBlock = ComponentMedia | ComponentQuote | ComponentRichText | ComponentSlider;
 
 export interface StrapiResponse<T> {
   data: T;
@@ -19,8 +43,3 @@ export interface StrapiResponse<T> {
     };
   };
 }
-
-export type Article = StrapiItem<ApiArticleArticle>;
-export type Author = StrapiItem<ApiAuthorAuthor>;
-export type Category = StrapiItem<ApiCategoryCategory>;
-export type About = StrapiItem<ApiAboutAbout>;

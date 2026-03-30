8a667ed4a8206ce6defcd491f9f20b1cd89a6ded
Dejan Vjestica <dvjestica@gmail.com>
2026-03-30 13:07:54 +0200

    Add quality attr to loader

--- src/lib/strapi-loader.ts
+++ src/lib/strapi-loader.ts
@@ -1,6 +1,6 @@
 import { ImageLoader, ImageLoaderProps } from "next/image";
 
-const strapiLoader: ImageLoader = ({ src, width }: ImageLoaderProps) => {
+const strapiLoader: ImageLoader = ({ src, width, quality }: ImageLoaderProps) => {
   const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_MEDIA_URL || process.env.NEXT_PUBLIC_STRAPI_URL;
   if (!STRAPI_URL) throw new Error("NEXT_PUBLIC_STRAPI_MEDIA_URL is not defined");
 
@@ -11,7 +11,7 @@ const strapiLoader: ImageLoader = ({ src, width }: ImageLoaderProps) => {
   const filename = srcParts.pop();
   const directory = srcParts.join("/");
 
-  const finalPath = prefix ? `${directory}/${prefix}_${filename}` : path;
+  const finalPath = prefix ? `${directory}/${prefix}_${filename}?w=${width}&q=${quality || 75}` : path;
   return `${STRAPI_URL}${finalPath}`;
 };
 

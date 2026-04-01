f81ab7bcc54575a445f9654c1a52dffdc5ef2450
Dejan Vjestica <dvjestica@gmail.com>
2026-04-01 11:03:38 +0200

    revert proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,33 +1,29 @@
-import { NextResponse, type NextRequest } from "next/server";
-import { i18nConfig, Locale } from "./i18n-config";
+import { NextResponse, type NextRequest } from 'next/server';
+import { i18nConfig, Locale } from './i18n-config';
 
 export function proxy(request: NextRequest) {
-  const userAgent = request.headers.get("user-agent") || "";
+	const { pathname } = request.nextUrl;
+	const { locales, defaultLocale } = i18nConfig;
 
-  if (userAgent.includes("facebookexternalhit") || userAgent.includes("Twitterbot") || userAgent.includes("LinkedInBot") || userAgent.includes("Slackbot")) {
-    return NextResponse.next();
-  }
+	if (pathname.includes('.') || pathname.startsWith('/_next') || pathname.startsWith('/api')) {
+		return NextResponse.next();
+	}
+	const pathLocale = locales.find((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`) as
+		| Locale
+		| undefined;
 
-  const { pathname } = request.nextUrl;
-  const { locales, defaultLocale } = i18nConfig;
+	if (pathLocale === defaultLocale) {
+		const cleanPath = pathname.replace(`/${defaultLocale}`, '') || '/';
+		return NextResponse.redirect(new URL(cleanPath, request.url));
+	}
 
-  if (pathname.includes(".") || pathname.startsWith("/_next") || pathname.startsWith("/api")) {
-    return NextResponse.next();
-  }
-  const pathLocale = locales.find((locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`) as Locale | undefined;
+	if (!pathLocale) {
+		return NextResponse.rewrite(new URL(`/${defaultLocale}${pathname}`, request.url));
+	}
 
-  if (pathLocale === defaultLocale) {
-    const cleanPath = pathname.replace(`/${defaultLocale}`, "") || "/";
-    return NextResponse.redirect(new URL(cleanPath, request.url));
-  }
-
-  if (!pathLocale) {
-    return NextResponse.rewrite(new URL(`/${defaultLocale}${pathname}`, request.url));
-  }
-
-  return NextResponse.next();
+	return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+	matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };

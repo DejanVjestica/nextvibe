cbcaf0763f451371be8b5f776523ecdaaa111cf2
Dejan Vjestica <dvjestica@gmail.com>
2026-04-01 11:18:37 +0200

    refactor alternates

--- src/app/[locale]/about/layout.tsx
+++ src/app/[locale]/about/layout.tsx
@@ -1,23 +1,23 @@
-import { Metadata } from "next";
+import { Metadata } from 'next';
+import { getAlternateLanguages } from '@/lib/i18n-utils';
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }): Promise<Metadata> {
-  const { locale } = await params;
-  const canonicalUrl = locale === "de" ? "" : "/de";
+	const { locale } = await params;
+	const localizedUrl = getAlternateLanguages('about')[locale];
 
-  return {
-    title: locale === "de" ? "NextVibe - Über uns" : "NextVibe - About",
-    description: `${locale === "de" ? "Über NextVibe: Erfahren Sie mehr über unsere Mission und unser Team." : "About page for NextVibe, learn more about our mission and team."}`,
-    alternates: {
-      canonical: `${canonicalUrl}/about`,
-      languages: {
-        de: "/about",
-        en: "/en/about",
-        "x-default": "/about",
-      },
-    },
-  };
+	return {
+		title: locale === 'de' ? 'NextVibe - Über uns' : 'NextVibe - About',
+		description: `${locale === 'de' ? 'Über NextVibe: Erfahren Sie mehr über unsere Mission und unser Team.' : 'About page for NextVibe, learn more about our mission and team.'}`,
+		alternates: {
+			canonical: localizedUrl,
+			languages: {
+				...getAlternateLanguages('about'),
+				'x-default': localizedUrl,
+			},
+		},
+	};
 }
 
 export default async function AboutLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+	return <>{children}</>;
 }
--- src/lib/i18n-utils.ts
+++ src/lib/i18n-utils.ts
@@ -1,19 +1,33 @@
-import { i18nConfig, Locale } from "@/i18n-config";
+import { i18nConfig, Locale } from '@/i18n-config';
 
-export function getLocalizedPath(pathname: string, newLocale: string) {
-  const segments = pathname.split("/").filter(Boolean);
-  const { locales, defaultLocale, prefixDefault } = i18nConfig;
+export const getLocalizedPath = (pathname: string, newLocale: string) => {
+	const segments = pathname.split('/').filter(Boolean);
+	const { locales, defaultLocale, prefixDefault } = i18nConfig;
 
-  if (locales.includes(segments[0] as Locale)) {
-    segments.shift();
-  }
-  let newPath: string;
+	if (locales.includes(segments[0] as Locale)) {
+		segments.shift();
+	}
+	let newPath: string;
 
-  if (newLocale === defaultLocale && !prefixDefault) {
-    newPath = "/" + segments.join("/");
-  } else {
-    newPath = "/" + newLocale + "/" + segments.join("/");
-  }
+	if (newLocale === defaultLocale && !prefixDefault) {
+		newPath = '/' + segments.join('/');
+	} else {
+		newPath = '/' + newLocale + '/' + segments.join('/');
+	}
 
-  return newPath;
-}
+	return newPath;
+};
+
+export const getBaseUrl = () =>
+	process.env.NEXT_PUBLIC_SITE_URL ??
+	(process.env.NODE_ENV === 'development' ? `http://localhost:${process.env.PORT || 3000}` : undefined);
+
+export const getAlternateLanguages = (slug: string = '') => {
+	const slugStr = slug ? `/${slug}` : '';
+	return Object.fromEntries(
+		i18nConfig.locales.map((lang) => [
+			lang,
+			lang === i18nConfig.defaultLocale ? `${getBaseUrl()}${slugStr}` : `${getBaseUrl()}/${lang}${slugStr}`,
+		]),
+	);
+};

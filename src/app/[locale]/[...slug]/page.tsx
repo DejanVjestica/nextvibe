2490394e8b5b543e01bb210db3decb079a727e9d
Dejan Vjestica <dvjestica@gmail.com>
2026-04-10 13:03:13 +0200

    FIx slug page prop type

--- src/app/[locale]/[...slug]/page.tsx
+++ src/app/[locale]/[...slug]/page.tsx
@@ -1,10 +1,8 @@
-import { Locale } from "@/i18n-config";
-
-export default async function Home({ params }: { params: Promise<{ slug: Locale }> }) {
-  const { slug } = await params;
-  return (
-    <div className="max-w-container mx-auto p-8">
-      <h1>Locale main: {slug}</h1>
-    </div>
-  );
+export default async function Home({ params }: { params: Promise<{ slug: string }> }) {
+	const { slug } = await params;
+	return (
+		<div className="max-w-container mx-auto p-8">
+			<h1>page slug: {slug}</h1>
+		</div>
+	);
 }

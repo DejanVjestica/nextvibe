713b05187f91b4562106752a61b70c7c0fd61808
Dejan Vjestica <dvjestica@gmail.com>
2026-04-23 17:26:46 +0200

    fix strapi-url

--- src/lib/config/strapi-url.ts
+++ src/lib/config/strapi-url.ts
@@ -4,8 +4,7 @@ export const strapiUrl = () => {
 	const url = trimTrailingSlash(process.env.NEXT_PUBLIC_STRAPI_URL || '');
 
 	if (url) return url;
-	if (process.env.NODE_ENV === 'development') return `http://localhost:1337`;
-	if (process.env.NODE_ENV === 'test') return `http://localhost:1337`;
+	if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') return `http://localhost:1337`;
 
 	throw new Error(`NEXT_PUBLIC_STRAPI_URL is not set in ${process.env.NODE_ENV} environment`);
 };

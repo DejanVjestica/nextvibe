84a81e85a8346c48b7e60e5094e6b124cbc37a3b
Dejan Vjestica <dvjestica@gmail.com>
2026-05-06 17:18:36 +0200

    add home service and test

---
+++ src/services/home.test.ts
@@ -0,0 +1,152 @@
+import { fetchStrapi } from '@/lib/strapi/fetch';
+import { getHome } from './home';
+
+vi.mock('@/lib/strapi/fetch', () => ({
+	fetchStrapi: vi.fn(),
+}));
+
+describe('getHome Service', () => {
+	beforeEach(() => {
+		vi.clearAllMocks();
+	});
+
+	it('calls fetchStrapi with the correct endpoint and locale', async () => {
+		vi.mocked(fetchStrapi).mockResolvedValue({
+			data: {
+				id: 2,
+				documentId: 'frqhnrreev16jmtxbakgts2b',
+				createdAt: '2026-04-30T09:36:01.168Z',
+				updatedAt: '2026-04-30T12:14:43.188Z',
+				publishedAt: '2026-04-30T12:14:43.204Z',
+				locale: 'en',
+				hero: {
+					eyebrow: null,
+				},
+			},
+			meta: {},
+		});
+
+		await getHome('en');
+
+		expect(fetchStrapi).toHaveBeenCalledWith('/home', {
+			populate: {
+				hero: {
+					populate: {
+						image: {
+							populate: '*',
+						},
+						primaryCta: {
+							populate: {
+								icon: {
+									populate: '*',
+								},
+							},
+						},
+						secondaryCta: {
+							populate: {
+								icon: {
+									populate: '*',
+								},
+							},
+						},
+					},
+				},
+			},
+			locale: 'en',
+		});
+	});
+
+	it('returns parsed home data when content exists', async () => {
+		vi.mocked(fetchStrapi).mockResolvedValue({
+			data: {
+				id: 9,
+				documentId: 'frqhnrreev16jmtxbakgts2b',
+				createdAt: '2026-04-30T09:36:01.168Z',
+				updatedAt: '2026-04-30T12:14:43.188Z',
+				publishedAt: '2026-04-30T12:14:43.204Z',
+				locale: 'de',
+				hero: {
+					id: 9,
+					eyebrow: 'lorem ipsum 1',
+					title: 'Main page',
+					subtitle: 'Lorem ipsum dolor',
+					variant: 'main',
+					image: null,
+					primaryCta: {
+						id: 16,
+						label: 'asddsadsa',
+						as: 'button',
+						href: null,
+						openInNewTab: false,
+						kind: 'primary',
+						ariaLabel: 'ddsadsads',
+						size: 'large',
+					},
+					secondaryCta: {
+						id: 17,
+						label: 'Learn more',
+						as: 'link',
+						href: 'https://developer.mozilla.org/en-US/',
+						openInNewTab: true,
+						kind: 'secondary',
+						ariaLabel: 'Learn more',
+						size: 'large',
+					},
+				},
+			},
+			meta: {},
+		});
+
+		const result = await getHome('de');
+
+		expect(result).toEqual({
+			id: 9,
+			hero: {
+				eyebrow: 'lorem ipsum 1',
+				title: 'Main page',
+				subtitle: 'Lorem ipsum dolor',
+				variant: 'main',
+				image: null,
+				primaryCta: {
+					label: 'asddsadsa',
+					ariaLabel: 'ddsadsads',
+					as: 'button',
+					href: null,
+					openInNewTab: false,
+					kind: 'primary',
+					size: 'large',
+					icon: null,
+				},
+				secondaryCta: {
+					label: 'Learn more',
+					ariaLabel: 'Learn more',
+					as: 'link',
+					href: 'https://developer.mozilla.org/en-US/',
+					openInNewTab: true,
+					kind: 'secondary',
+					size: 'large',
+					icon: null,
+				},
+			},
+		});
+	});
+
+	it('returns null when hero do not exist', async () => {
+		vi.mocked(fetchStrapi).mockResolvedValue({
+			data: {
+				id: 2,
+				documentId: 'frqhnrreev16jmtxbakgts2b',
+				createdAt: '2026-04-30T09:36:01.168Z',
+				updatedAt: '2026-04-30T12:14:43.188Z',
+				publishedAt: '2026-04-30T12:14:43.204Z',
+				locale: 'en',
+				hero: null,
+			},
+			meta: {},
+		});
+
+		const result = await getHome('en');
+
+		expect(result).toBeNull();
+	});
+});
---
+++ src/services/home.ts
@@ -0,0 +1,44 @@
+import { fetchStrapi } from '@/lib/strapi/fetch';
+import { parseHome } from '@/lib/strapi/parse-home';
+import { Home } from '@/lib/strapi/types';
+import { ParsedHome } from '@/lib/strapi/types/parsed-home';
+
+const populate = {
+	hero: {
+		populate: {
+			image: {
+				populate: '*',
+			},
+			primaryCta: {
+				populate: {
+					icon: {
+						populate: '*',
+					},
+				},
+			},
+			secondaryCta: {
+				populate: {
+					icon: {
+						populate: '*',
+					},
+				},
+			},
+		},
+	},
+};
+
+export const getHome = async (locale: string): Promise<ParsedHome | null> => {
+	const res = await fetchStrapi<Home>('/home', {
+		populate,
+		locale,
+	});
+
+	const { id, hero } = parseHome(res.data);
+
+	if (!hero) return null;
+
+	return {
+		id,
+		hero,
+	};
+};
